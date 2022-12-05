c93cd7883272c4067877582250542a685eb494b2
sunshine970918 <76559888+sunshine970918@users.noreply.github.com>
2022-12-05 20:07:04 +0800

    Update substore.js

--- Script/substore.js
+++ Script/substore.js
@@ -74,7 +74,7 @@ const defaultNetworkPath = getVal('defaultNetworkPath') || '/'
 /* 节点名前缀 */
 const prefix = getVal('prefix') || '免流 '
 /* 节点名后缀 */
-const suffix = getVal('suffix') || '混淆 '
+const suffix = getVal('suffix') || ''
 /* 附加 Host 前缀 */
 const hostPrefix = getVal('hostPrefix') || ''
 const hostSuffix = getVal('hostSuffix') || ''
